ade529eb5ba5094205dc63a7a38c3cd2df3c81b5
Aaquib Faridi <aaquib.faridi@nagarro.com>
2021-12-05 22:13:12 +0530

    first commit

---
+++ static/js/28.4e6c8cb0.chunk.js
@@ -0,0 +1,1 @@
+(this.webpackJsonplastArzi=this.webpackJsonplastArzi||[]).push([[28],{586:function(t,e,n){"use strict";n.r(e);var a=n(0),r=n.n(a),i=n(405);e.default=function(){return r.a.createElement(i.a,{md:"12",style:{marginTop:"70px",textAlign:"center"}},"Privacy")}}}]);
